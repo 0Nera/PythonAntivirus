64a01487c68251e6a3c5130e6dc76f27563ac564
0Nera <71536443+0Nera@users.noreply.github.com>
2021-08-11 17:36:32 +0300

    Add file reader

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 # Python program to find SHA256 hash string of a file
 import hashlib
+import os
 
-list_hash = [
-    "a300759b1e3f5e8e7996b544cdb4e5b6b2bd3ebe6d45d105ce92e86e4ee06569"
-]
+list_hash = []
+
+with open("list", "r") as ins:
+    for line in ins:
+        list_hash.append(line)
 
 def init():
     try:
         filename = input("Enter the input file name: ")
         sha256_hash = hashlib.sha256()
+
         with open(filename,"rb") as f:
             # Read and update hash string value in blocks of 4K
             for byte_block in iter(lambda: f.read(4096),b""):
                 sha256_hash.update(byte_block)
+
             for i in list_hash:
                 if i == sha256_hash.hexdigest():
                     print("File is virus!")
                     break;
-            print(sha256_hash.hexdigest())
+            print("SHA256: ", sha256_hash.hexdigest())
             
     except Exception as e:
         print(e)
         init()
+
+list = os.listdir("./virus/")
+number_files = len(list)
+print("Infected: ", number_files)
+print("Database: ", len(list_hash))
 
 
 init()
